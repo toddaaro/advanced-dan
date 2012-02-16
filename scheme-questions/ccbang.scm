5cfb1c080b6c4d14800dde5b57fc1ddcc1f24ba0
Aaron Todd <github@opprobrio.us>
2012-02-16 13:53:09 -0800

    added the ccbang scheme question

---
+++ scheme-questions/ccbang.scm
@@ -0,0 +1,9 @@
+
+;; Why does the following run?
+;; More specifically, how does x know that in the future it will point to a box?
+
+(define run
+  (lambda ()
+    (x (call/cc (lambda (k)
+                  (set! x (lambda (h) 120))
+                  (k 'hukarz))))))
