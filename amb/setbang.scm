9702206fa378d730e6e12239449139b8cacc7d5c
Aaron Todd <github@opprobrio.us>
2012-02-21 10:25:44 -0800

    added a weeiiirrrddd setbang example

---
+++ amb/setbang.scm
@@ -0,0 +1,18 @@
+> (amb (omega) (+ 5 (call/cc (lambda (k) (set! y k) 120))))
+125
+> (set! x (amb (omega) (+ 5 (call/cc (lambda (k) (set! y k) 120)))))
+> (y x)
+> x
+130
+> (y x)
+> x
+135
+> (set! xx (+ 5 (call/cc (lambda (k) (set! yy k) 120))))
+> xx
+125
+> (yy xx)
+> xx
+130
+> (yy xx)
+> xx
+135
