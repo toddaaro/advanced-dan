ada596fd5f46faeb1fe5a1958ea42c6bad2a3b87
Aaron Todd <github@opprobrio.us>
2012-02-26 11:27:05 -0800

    added two simple tests to check for memory leaks now appears that I am leak free

--- delim/delim.scm
+++ delim/delim.scm
@@ -32,4 +32,39 @@
                                                       (lambda () ((unbox (cdr (assoc reset-id rks))) ebody))))))))))))]))
 
 
+;;; Tests
+;;;  - Need to test the hypothesis that the fluid-lets eliminate memory leaks due to global data strutures
+;;;  - These appear to confirm it,
+;;;    - If (collect (collect-maximum-generation)) is called after a run there is no net memory consumption increase
+
+
+(define MB (expt 2 20))
+
+(define test-mem
+  (lambda (x)
+    (letrec ([loop (lambda (n)
+                     (cond
+                       [(zero? n) 120]
+                       [else
+                        (begin (reset
+                                (bytevector=? (make-bytevector (* 100 MB) (random 255)) (shift (lambda (k)
+                                                   (k (make-bytevector (* 100 MB) (random 255)))))))
+                               (loop (sub1 n)))]))])
+      (loop x))))
+                     
+(define test-mem2
+  (lambda (x)
+    (letrec ([loop (lambda (n)
+                     (cond
+                       [(zero? n) 120]
+                       [else
+                        (begin
+                          (reset
+                           (or #f
+                               (reset
+                                (bytevector=? (make-bytevector (* 100 MB) (random 255))
+                                              (shift 1 (lambda (k)
+                                                         (k (make-bytevector (* 100 MB) (random 255)))))))))
+                          (loop (sub1 n)))]))])
+      (loop x))))
 
