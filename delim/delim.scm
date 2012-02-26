d80b0175b627023825bab3630805e13be798bf2b
Aaron Todd <github@opprobrio.us>
2012-02-25 16:24:46 -0800

    fixed delim so that continuations can be invoked outside resets, need to figure out if the fluid-let nonsense is actually preventing memory leaks like I was hoping

--- delim/delim.scm
+++ delim/delim.scm
@@ -4,14 +4,11 @@
 ;;;   - Allows for arbitrary numbers of shifts and resets
 ;;;   - If an id is given to shift it jumps to the reset with corresponding depth
 ;;;   - If no id is given it pairs with the nearest reset
+;;;   - continuations can be moved to the "outside world" and invoked without issue (was the goal...so?)
 
 (define reset-id 0)
 (define rks '())
 (define jks '())
-
-(define update-rk
-  (lambda (id k)
-    (set-box! (cdr (assoc id rks)) k)))
 
 (define-syntax reset
   (syntax-rules ()
@@ -20,15 +17,19 @@
                 (fluid-let ([reset-id (add1 reset-id)])
                   (fluid-let ([rks (cons (cons reset-id (box jk)) rks)]
                               [jks (cons (cons reset-id jk) jks)])
-                      (fluid-let-syntax ([shift (syntax-rules ()
-                                                  [(_ id f)
-                                                   (call/cc (lambda (k)
-                                                              ((cdr (assoc id jks)) (f (lambda (arg)
-                                                                                         (call/cc (lambda (ik)
-                                                                                                    (update-rk id ik)
-                                                                                                    (k arg))))))))]
-                                                  [(_ f) (shift reset-id f)])])
-                                        (let ([ebody body])
-                                          ((unbox (cdr (assoc reset-id rks))) ebody)))))))]))
+                    ((call/cc (lambda (uk)
+                                (let ([update-rk (lambda (id k) (call/cc (lambda (outk)
+                                                                           (uk (lambda () (set-box! (cdr (assoc id rks)) k) (outk))))))])
+                                  (fluid-let-syntax ([shift (syntax-rules ()
+                                                              [(_ id f)
+                                                               (call/cc (lambda (k)
+                                                                          ((cdr (assoc id jks)) (f (lambda (arg)
+                                                                                                     (call/cc (lambda (ik)
+                                                                                                                (update-rk id ik)
+                                                                                                                (k arg))))))))]
+                                                              [(_ f) (let ([nid reset-id]) (shift nid f))])])
+                                                    (let ([ebody body])
+                                                      (lambda () ((unbox (cdr (assoc reset-id rks))) ebody))))))))))))]))
+
 
 
