89e5aa09851c60689e393f476b4b660a3e330434
Aaron Todd <github@opprobrio.us>
2012-02-28 11:44:14 -0800

    nested shifts paired with the same reset work bang

--- delim/delim.scm
+++ delim/delim.scm
@@ -9,27 +9,82 @@
 (define reset-id 0)
 (define rks '())
 (define jks '())
+(define shift-depth 0)
+(define shifted '())
+
+(define-syntax shift
+  (syntax-rules ()
+    [(_ body) (fshift body)]
+    [(_ n body) (fshift n body)]))
+
+(define fshift
+  (case-lambda
+    [(n f) 'hukarz]
+    [(f) 'hukarz]))
 
 (define-syntax reset
   (syntax-rules ()
     [(_ body)
      (call/cc (lambda (jk)
                 (fluid-let ([reset-id (add1 reset-id)])
-                  (fluid-let ([rks (cons (cons reset-id (box jk)) rks)]
-                              [jks (cons (cons reset-id jk) jks)])
+                  (fluid-let ([rks (cons (cons reset-id (box '())) rks)]
+                              [jks (cons (cons reset-id (box `(,jk))) jks)])
                     ((call/cc (lambda (uk)
-                                (let ([update-rk (lambda (id k) (call/cc (lambda (outk)
-                                                                           (uk (lambda () (set-box! (cdr (assoc id rks)) k) (outk))))))])
-                                  (fluid-let-syntax ([shift (syntax-rules ()
-                                                              [(_ id f)
+                                (let* ([push-rk (lambda (id k) (call/cc (lambda (outk)
+                                                                           (uk (lambda () (pretty-print rks) (set-box! (cdr (assoc id rks)) (cons k (unbox (cdr (assoc id rks))))) (outk))))))]
+
+                                       [push-jk (lambda (id k) (call/cc (lambda (outk)
+                                                                           (uk (lambda () (pretty-print jks) (set-box! (cdr (assoc id jks)) (cons k (unbox (cdr (assoc id jks))))) (outk))))))]
+
+                                       
+                                       
+                                       [apply-jk (lambda (id ebody) (if (not (null? (unbox (cdr (assoc id jks)))))
+                                                                        (let ([ks (unbox (cdr (assoc id jks)))])
+                                                                          (set-box! (cdr (assoc id jks)) (cdr ks))
+                                                                          ((car ks) ebody))
+                                                                        (jk ebody)))]
+                                       [apply-rk (lambda (id ebody) (pretty-print "about to appply-rk") (pretty-print id) (pretty-print rks) (if (not (null? (unbox (cdr (assoc id rks)))))
+                                                                                                                                                 (let ([ks (unbox (cdr (assoc id rks)))])
+                                                                                                                                                   
+                                                                                                                                                   (set-box! (cdr (assoc id rks)) (cdr ks))
+                                                                                                                                                   (pretty-print rks)
+                                                                                                                                                   ((car ks) ebody))
+                                                                                                                                                 (apply-jk id ebody)))]
+                                       )
+                                  
+                                  
+                                  
+                                  (fluid-let ([fshift (case-lambda 
+                                                              [(id f)
+                                                               (let ([sid (gensym)])
+                                                               (fluid-let ([shift-depth (add1 shift-depth)]
+                                                                           [shifted (cons (cons sid (box #f)) shifted)]
+                                                                           
+                                                                           )
                                                                (call/cc (lambda (k)
-                                                                          ((cdr (assoc id jks)) (f (lambda (arg)
+
+
+                                                                          
+                                                                          
+                                                                          ;(pretty-print "about to appply jumpout")
+                                                                          ;(pretty-print id)
+                                                                          ;(pretty-print jks)
+                                                                          (pretty-print "what is the shift-depth")
+                                                                          (pretty-print shift-depth)
+                                                                          (let ([work (f (lambda (arg)
                                                                                                      (call/cc (lambda (ik)
-                                                                                                                (update-rk id ik)
-                                                                                                                (k arg))))))))]
-                                                              [(_ f) (let ([nid reset-id]) (shift nid f))])])
+                                                                                                                (if (and (> shift-depth 1) (not (unbox (cdr (assoc sid shifted)))))
+                                                                                                                    (begin (set-box! (cdr (assoc sid shifted)) #t) (push-jk id ik))
+                                                                                                                    (push-rk id ik))
+                                                                                                                (k arg)))))])
+                                                                          (if (equal? 1 shift-depth)
+                                                                              (apply-jk id work)
+                                                                              (apply-rk id work)))))))]
+
+                                                                                    
+                                                              [(f) (let ([nid reset-id]) (fshift nid f))])])
                                                     (let ([ebody body])
-                                                      (lambda () ((unbox (cdr (assoc reset-id rks))) ebody))))))))))))]))
+                                                      (lambda () (apply-rk reset-id ebody))))))))))))]))
 
 
 ;;; Tests
@@ -51,7 +106,7 @@
                                                    (k (make-bytevector (* 100 MB) (random 255)))))))
                                (loop (sub1 n)))]))])
       (loop x))))
-                     
+
 (define test-mem2
   (lambda (x)
     (letrec ([loop (lambda (n)
@@ -59,12 +114,64 @@
                        [(zero? n) 120]
                        [else
                         (begin
-                          (reset
-                           (or #f
-                               (reset
-                                (bytevector=? (make-bytevector (* 100 MB) (random 255))
-                                              (shift 1 (lambda (k)
-                                                         (k (make-bytevector (* 100 MB) (random 255)))))))))
+                          (add1 
+                           (reset
+                            (bytevector=? (make-bytevector (* 100 MB) (random 255))
+                                          (shift 1 (lambda (k)
+                                                     (k (make-bytevector (* 100 MB) (random 255)))
+                                                     4)))))
                           (loop (sub1 n)))]))])
       (loop x))))
 
+;;; Basic tests
+
+(load "../utils/tester.scm")
+
+(define run-tests
+  (lambda ()
+
+    (test-check "identity k"
+      (reset (shift (lambda (k) (k 120))))
+      120)
+
+    (test-check "apply k twice"
+      (reset (+ 10 (shift (lambda (k) (k (k 100))))))
+      120)
+
+    (test-check "dynamic, first reset"
+      (reset (+ 100 (reset (+ 10 (shift (lambda (k) (k (k 0))))))))
+      120)
+
+    (test-check "dynamic numeric id 1"
+      (reset (+ 50 (reset (+ 10 (shift 1 (lambda (k) (k (k 0))))))))
+      120)
+
+    (test-check "dynamic numeric id > 1"
+      (reset (+ 100 (reset (+ 10 (shift 2 (lambda (k) (k (k 0))))))))
+      120)
+
+    (test-check "k outside reset"
+      (let ([madd (reset (+ 10 (shift (lambda (k) k))))])
+        (reset (+ 100 (shift (lambda (k) (madd (k (madd 0))))))))
+      120)
+
+    (test-check "shift without reset in function"
+      (let ([twice (lambda (x) (shift (lambda (k) (k (k x)))))])
+        (reset (+ 10 (twice 100))))
+      120)
+
+    (test-check "shift outside reset returning k outside reset"
+      (let* ([twice (lambda () (shift (lambda (k) (lambda (x) (k (k x))))))]
+             [madd (reset (+ 10 (twice)))])
+        (madd 100))
+      120)
+
+
+      
+
+    ))
+
+      
+;(reset (+ 1 (shift (lambda (k)
+;                     (k (k (reset (+ 10 (shift 1 (lambda (kk)
+;                                                   (kk (kk 0))))))))))))
